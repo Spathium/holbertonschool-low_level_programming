7a1380f430ac60074591f936f7075b0592b2d9ee
Meliza Posada <spathium@MacBook-Air-de-Meliza.local>
2022-06-16 22:36:03 -0500

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	char low;
+	char alpha;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
 		putchar('\n');
 
 	return (0);
