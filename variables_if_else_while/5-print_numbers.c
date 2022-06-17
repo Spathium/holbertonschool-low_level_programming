fa1f01dc71cc1726e20042a96f9bf924c3544f6c
Meliza Posada <spathium@MacBook-Air-de-Meliza.local>
2022-06-17 17:27:25 -0500

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ent;
+
+	for (ent = 0; ent <= 9; ent++)
+	printf("%d", ent);
+	printf("\n");
+
+	return (0);
+}
