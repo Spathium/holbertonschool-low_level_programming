a7631c544c068fbe50de2ce3a0200b36a652c0cc
Meliza Posada <spathium@Spathium.local>
2022-06-21 14:50:27 -0500

    sum two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - numbers
+ * @b: int b
+ * @a: int a
+ * Description: sum two integers and returns the result
+ * Return: Result to add a and b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
