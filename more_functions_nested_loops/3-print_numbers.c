7d5e29d43acdc28d192c0a165df249ff67b4d9bd
Meliza Posada <spathium@Spathium.local>
2022-06-28 16:25:20 -0500

    prints numbers 0-9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	char i;
+	
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+}
