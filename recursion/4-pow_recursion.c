b83093ab257581cf0d685aae9c66940ebf1633a8
Meliza Posada <spathium@Spathium.local>
2022-07-01 13:06:12 -0500

    sqrt

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *_pow_recursion - return the vale of variable x
+ * _pow_recursion - It returns the value of x raised to the power y.
  * @x: variable
  * @y: variable
  * Return 0
 */
 
 int _pow_recursion(int x, int y)
+
 {
 	if (y == 0)
 		return (1);
