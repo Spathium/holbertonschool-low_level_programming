93b936f44d25252a4398bc38f3d36a9f0f746001
Meliza Posada <spathium@Spathium.local>
2022-06-30 22:22:09 -0500

    pow

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Returns the value of x 
+ * @x: variable
+ * @y: variable
+ * Return 0
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	return (x *= _pow_recursion(x, y - 1));
+}
