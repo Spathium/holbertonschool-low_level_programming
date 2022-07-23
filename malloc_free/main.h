f0f9d8d1d7c4bb7b5a6f1b25052e4bd1bff2f14f
Meliza Posada <spathium@Spathium.local>
2022-07-23 13:24:09 -0500

    101

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
 char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
